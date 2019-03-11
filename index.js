0644880b07ce86fa9d5a22fe0d1b679657c88c5b
Dana Simmons <dana@simmons-audio.com>
2019-03-11 11:59:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file.
+
+
+function lowerCaseDrivers(drivers) {
+    return drivers.map(d => d.toLowerCase())
+}
+
+function nameToAttributes(drivers) {
+    let obj = {}
+    obj = drivers.map(function(d) {
+        let name = d.split(" ")
+        console.log(name)
+        return {
+            firstName: name[0],
+            lastName: name[1]
+        }
+    })
+    return obj
+}
+
+function attributesToPhrase(drivers) {
+    return drivers.map(d => `${d.name} is from ${d.hometown}`)
+}
